8e4d8a866c19061201a14224d39826ad67791549
Ian Abenes <40700608+boredStats@users.noreply.github.com>
2018-12-18 01:10:46 -0600

    added final function version (privatized)

--- super_corr.py
+++ super_corr.py
@@ -1,13 +1,21 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Nov  2 11:08:52 2018
+Created on Fri Nov  9 10:28:58 2018
 
-Function for voxelwise connectivity analysis
-
-@author: ixa080020
+Updates:
+    Batched to cluster
+    Running batches from __main__
+    Privatized paths
 """
+
+import os
+import h5py
 import numpy as np
+import nilearn
 import datetime
+import pandas as pd
+from nilearn.input_data import NiftiMasker as masker
+nilearn.EXPAND_PATH_WILDCARDS = False
 
 def center_matrix(a):
     """
@@ -16,9 +24,10 @@ def center_matrix(a):
     mu_a = a.mean(0)    
     return np.subtract(a,np.reshape(np.repeat(mu_a,a.shape[0]),a.shape,order="F"))
 
-def generate_correlation_map(x,y,h5pyFile=None,datasetName=None,memory_check=True):
-    """Correlate each column of X with each column of Y.
-
+def generate_correlation_map(x,y,h5pyFile=None,datasetName=None,small_matrices=False):
+    """
+    Function for correlating two matrices with uneven number of columns
+    
     Parameters
     ----------
     x : np.array
@@ -34,29 +43,16 @@ def generate_correlation_map(x,y,h5pyFile=None,datasetName=None,memory_check=Tru
     datasetName : string
       If no name is provided, the dataset will automatically be named
     
-    memory_check : bool
+    small_matrices : bool
       Flag for memory operations
-        True: assumes that x and y are large matrices
-        False: x and y are relatively small matrices
+        True: override h5py requirement <-- thar be danger matey
+        False: x and y are large matrices
         
-      If you have large matrices (large N and/or M and/or T), it is recommended
-      that you provide a path to a hdf5 file
-    
-    Returns
-    -------
-    np.array
-      N X M array in which each element is a correlation coefficient.
-
-    Formula
-    -------
-    First, center x and y. Then 
-      cov = np.dot(x, y.T)
-      
-      r = cov / np.dot(std_x[:, np.newaxis], std_y[np.newaxis, :])  
+      If you have large matrices, provide a hdf5 file
     """
-    n = x.shape[1]
-    m = y.shape[1]
-    s = x.shape[0]
+    n = x.shape[1] #numcols of x
+    m = y.shape[1] #numcols of y
+    s = x.shape[0] #num subjects/timepoints
     
     if s != y.shape[0]:
         raise ValueError ("x and y must have the same number of observations")
@@ -64,32 +60,32 @@ def generate_correlation_map(x,y,h5pyFile=None,datasetName=None,memory_check=Tru
     std_x = x.std(0, ddof=s - 1)
     std_y = y.std(0, ddof=s - 1)
     
-    if h5pyFile==None and memory_check==True: 
-        if n<10000 and m<10000 and s<1000:
+    if h5pyFile==None: 
+        if n<12000 and m<12000 and s<1200:
+            cov = np.dot(center_matrix(x).T,center_matrix(y))
+            return cov/np.dot(std_x[:, np.newaxis], std_y[np.newaxis, :])
+        elif small_matrices==True:
             cov = np.dot(center_matrix(x).T,center_matrix(y))
             return cov/np.dot(std_x[:, np.newaxis], std_y[np.newaxis, :])
         else:
-            raise OverflowError ("Input matrices are probably too large")
-    
+            raise OverflowError("Inputs are probably too large, provide a hdf5 file")
+            
     if datasetName==None:
         datasetName = "corrdata_%s" % datetime.datetime.today()
          
     cov = np.dot(center_matrix(x).T,center_matrix(y))
     r = cov/np.dot(std_x[:, np.newaxis], std_y[np.newaxis, :])
     del cov
-
-    h5pyFile.create_dataset(
+    f = h5py.File(h5pyFile,"a")
+    f.create_dataset(
             datasetName,
             shape=(x.shape[1], y.shape[1]),
             dtype="f",
-#                compression="gzip",
             compression="lzf",
-#                chunks=(100,y.shape[1]),
-            data=r
-            )
+            data=r)
+    f.close()
     del r
-    return h5pyFile
-
+    
 def chunk_getter(maxcol, chunk_size=1000):
     """
     Calculate number of chunks to divide x_cols into
@@ -109,40 +105,51 @@ def colrange_getter(maxcol, chunk, chunk_size=1000):
         colrange = range(chunk*chunk_size, maxcol)
     return colrange
 
-if __name__ == "__main__":
-# Testing performance
-    import h5py
-    import nilearn
-    from nilearn.input_data import NiftiMasker as masker
-    nilearn.EXPAND_PATH_WILDCARDS = False
-    
-    test_file = "test_scan.nii.gz"
-    output_file = "test_subject_super_corr2.hdf5"
-    
-    f = h5py.File(output_file, "w")
-    print("Creating hdf5 file: %s" % datetime.datetime.now())
-    f.close()
-    
-    brain_masker = masker(verbose=0)
-    cing_masker = masker("aal2_cingulate.nii.gz",verbose=0)
-    
-    brain_ts = brain_masker.fit_transform(test_file)
-    cing_ts = cing_masker.fit_transform(test_file)
-    print("Masking data: %s" % datetime.datetime.now())
-    
-    chunk_size = 3000
-    chunks = chunk_getter(cing_ts.shape[1], chunk_size)
-    
-    for chunk in range(chunks):
-        key = "cingulate_chunk_%03d" % chunk
-        colrange = colrange_getter(cing_ts.shape[1], chunk, chunk_size)
+def cingulate_voxelwise():
+    """
+    First-level voxel-voxel correlations for the cingulate cortex
+    """  
+    #Batch code
+    with open("privatePaths.txt", 'r') as f:
+        paths = [str(k).rstrip("\n") for k in f]
+    subj_dir = r"%s" % paths[0]
+    output_path = r"%s" % paths[1]
         
-        f = h5py.File(output_file, "r+")
-        generate_correlation_map(cing_ts[:,colrange], brain_ts, f, key)
-        f.close()
-        print("Calc/save for chunk %d: %s" % (chunk, datetime.datetime.now()))
-    
-#    from multiprocessing import Pool  
-#    chunks = chunk_getter(cing_ts.shape[1])
-#    p = Pool()
-#    p.map(generate_correlation_map(cing_ts[:,colrange_getter(cing_ts.shape[1],chunk)],brain_ts,f) for chunk in range(chunks))
+    chunk_dir =  r"%s" % paths[2]
+    subj_batch = pd.read_csv("batch_1.csv",header=None,index_col=False)
+
+    for s in subj_batch.values:
+        s_str = str(s)
+        subj = s_str.replace("[","").replace("'","").replace("]","")
+        
+        #Filename structure: denoised_######.nii
+        subj_code = str(subj).split("_")[1].replace(".nii","")
+        subj_nifti = os.path.join(subj_dir,subj)
+        #print(subj_nifti)
+        brain_masker = masker(verbose=1)
+        brain_ts = brain_masker.fit_transform(subj_nifti)
+        #print("Brain loaded")
+        
+        cing_files = os.listdir(chunk_dir)
+        for cing_file in cing_files:    
+            cing_mask = os.path.join(chunk_dir,cing_file)
+            cing_masker = masker(cing_mask, verbose=1)
+            cing_ts = cing_masker.fit_transform(subj_nifti)
+            
+            chunk_size = 200
+            chunks = chunk_getter(cing_ts.shape[1], chunk_size)         
+            
+            cing_name = str(cing_file).replace(".nii.gz","")
+            outfile = os.path.join(output_path, "%s_%s.hdf5" % (subj_code,cing_name))
+            for chunk in range(chunks):
+                colrange = colrange_getter(cing_ts.shape[1], chunk, chunk_size)
+                chunk_name = "columns %d to %d" % (min(colrange),max(colrange))
+                generate_correlation_map(cing_ts[:, colrange], brain_ts, outfile,chunk_name)
+            del cing_ts  
+            #break #performance testing
+        
+        del brain_ts
+        #break #performance testing
+        
+if __name__ == "__main__":
+    cingulate_voxelwise()
