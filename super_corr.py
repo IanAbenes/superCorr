8789623abb08049d9ae78f11a3f486f76211d205
Ian Abenes <40700608+boredStats@users.noreply.github.com>
2018-12-18 01:15:49 -0600

    added parallel processing version

--- super_corr.py
+++ super_corr.py
@@ -3,9 +3,7 @@
 Created on Fri Nov  9 10:28:58 2018
 
 Updates:
-    Batched to cluster
-    Running batches from __main__
-    Privatized paths
+    Re-added mpi4py components
 """
 
 import os
@@ -13,7 +11,6 @@ import h5py
 import numpy as np
 import nilearn
 import datetime
-import pandas as pd
 from nilearn.input_data import NiftiMasker as masker
 nilearn.EXPAND_PATH_WILDCARDS = False
 
@@ -50,6 +47,7 @@ def generate_correlation_map(x,y,h5pyFile=None,datasetName=None,small_matrices=F
         
       If you have large matrices, provide a hdf5 file
     """
+    
     n = x.shape[1] #numcols of x
     m = y.shape[1] #numcols of y
     s = x.shape[0] #num subjects/timepoints
@@ -60,16 +58,13 @@ def generate_correlation_map(x,y,h5pyFile=None,datasetName=None,small_matrices=F
     std_x = x.std(0, ddof=s - 1)
     std_y = y.std(0, ddof=s - 1)
     
-    if h5pyFile==None: 
+    if h5pyFile==None or small_matrices==True: 
         if n<12000 and m<12000 and s<1200:
-            cov = np.dot(center_matrix(x).T,center_matrix(y))
-            return cov/np.dot(std_x[:, np.newaxis], std_y[np.newaxis, :])
-        elif small_matrices==True:
             cov = np.dot(center_matrix(x).T,center_matrix(y))
             return cov/np.dot(std_x[:, np.newaxis], std_y[np.newaxis, :])
         else:
             raise OverflowError("Inputs are probably too large, provide a hdf5 file")
-            
+    
     if datasetName==None:
         datasetName = "corrdata_%s" % datetime.datetime.today()
          
@@ -85,7 +80,7 @@ def generate_correlation_map(x,y,h5pyFile=None,datasetName=None,small_matrices=F
             data=r)
     f.close()
     del r
-    
+
 def chunk_getter(maxcol, chunk_size=1000):
     """
     Calculate number of chunks to divide x_cols into
@@ -105,51 +100,65 @@ def colrange_getter(maxcol, chunk, chunk_size=1000):
         colrange = range(chunk*chunk_size, maxcol)
     return colrange
 
-def cingulate_voxelwise():
+def cingulate_voxelwise(subj_dir,subj_list,cing_dir,output_dir):
     """
     First-level voxel-voxel correlations for the cingulate cortex
-    """  
-    #Batch code
-    with open("privatePaths.txt", 'r') as f:
-        paths = [str(k).rstrip("\n") for k in f]
-    subj_dir = r"%s" % paths[0]
-    output_path = r"%s" % paths[1]
-        
-    chunk_dir =  r"%s" % paths[2]
-    subj_batch = pd.read_csv("batch_1.csv",header=None,index_col=False)
-
-    for s in subj_batch.values:
-        s_str = str(s)
-        subj = s_str.replace("[","").replace("'","").replace("]","")
-        
-        #Filename structure: denoised_######.nii
-        subj_code = str(subj).split("_")[1].replace(".nii","")
+    """        
+    for subj in subj_list:
+        print(subj)
         subj_nifti = os.path.join(subj_dir,subj)
-        #print(subj_nifti)
-        brain_masker = masker(verbose=1)
+
+        brain_masker = masker()
         brain_ts = brain_masker.fit_transform(subj_nifti)
-        #print("Brain loaded")
         
-        cing_files = os.listdir(chunk_dir)
+        cing_files = os.listdir(cing_dir)
         for cing_file in cing_files:    
-            cing_mask = os.path.join(chunk_dir,cing_file)
-            cing_masker = masker(cing_mask, verbose=1)
+            cing_mask = os.path.join(cing_dir,cing_file)
+        
+            cing_masker = masker(cing_mask)
             cing_ts = cing_masker.fit_transform(subj_nifti)
             
-            chunk_size = 200
-            chunks = chunk_getter(cing_ts.shape[1], chunk_size)         
-            
             cing_name = str(cing_file).replace(".nii.gz","")
-            outfile = os.path.join(output_path, "%s_%s.hdf5" % (subj_code,cing_name))
+            
+            chunk_size = 200
+            chunks = chunk_getter(cing_ts.shape[1], chunk_size)
+            
+            #Filename structure: denoised_######.nii
+            subj_code = str(subj).split("_")[1].replace(".nii","")
+            outfile = os.path.join(output_dir, "%s_%s.hdf5" % (subj_code,cing_name))
+            
             for chunk in range(chunks):
                 colrange = colrange_getter(cing_ts.shape[1], chunk, chunk_size)
                 chunk_name = "columns %d to %d" % (min(colrange),max(colrange))
-                generate_correlation_map(cing_ts[:, colrange], brain_ts, outfile,chunk_name)
-            del cing_ts  
-            #break #performance testing
-        
+                generate_correlation_map(cing_ts[:, colrange], brain_ts, outfile, chunk_name)
+            del cing_ts
         del brain_ts
-        #break #performance testing
         
 if __name__ == "__main__":
-    cingulate_voxelwise()
+    #Performance testing
+    from mpi4py import MPI
+    
+    comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()
+    size = comm.Get_size()
+    
+    with open("privatePaths.txt", 'r') as f:
+        paths = [str(k).rstrip("\n") for k in f]
+    
+    denoised_path = r"%s" % paths[0]
+    output_path = r"%s" % paths[1]
+
+    subj_files = [f for f in os.listdir(denoised_path) if f.endswith(".nii")][0:10]
+    print(subj_files)
+    num_files = int((len(subj_files))/size) #number of subj to process per node
+
+    mpi_chunk = [f for f in range(rank*num_files+1,(rank+1)*num_files+1)] #subj to process within node
+    print(mpi_chunk)
+    
+    subj_chunk = [subj_files[i-1] for i in mpi_chunk]
+    
+    chunk_dir = r"%s" % paths[2]
+  
+    print("Starting cingulate_voxelwise performance testing on 10 subjects: %s" % datetime.datetime.now())
+    cingulate_voxelwise(denoised_path, subj_chunk, chunk_dir, output_path)
+    print("Performance test end: %s" % datetime.datetime.now())
